58a71b0d91ad191cd7a64ab016098b74cfb35541
Jamie Weldon <jamie.weldon@outlook.com>
2024-03-19 05:10:09 -0500

    update to lib

---
+++ lib/ch5.dbtranscleanup.go
@@ -0,0 +1,28 @@
+package lib
+
+import (
+	"context"
+	"database/sql"
+)
+
+func DoSomeInserts(ctx context.Context, db *sql.DB, value1, value2 string) (err error) {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	_, err = tx.ExecContext(ctx, "INSERT INTO FOO (val) VALUES $1", value1)
+	if err != nil {
+		return err
+	}
+	// use txt to do more database inserts here
+	return nil
+}
